22f77324a9a22f2640a3e6ebe00bc4ad8861311d
John Kieran Palladino <johnpalladino0@gmail.com>
2023-02-26 13:36:35 -0500

    Implemented 2 Controllers

--- server/src/controllers/DataController.ts
+++ server/src/controllers/DataController.ts
@@ -13,20 +13,18 @@ Disconnect()
 
 */
 
-import { Buffer } from "https://deno.land/std@0.177.0/io/mod.ts";
-
-interface DataController {
+export interface DataController {
     ///
     /// Stores a file on the filesystem
     ///
     StoreInFile(
         file: string,
-        data: Buffer): Promise<void>;
+        data: Uint8Array): Promise<void>;
 
     ///
     /// Loads a file from the filesystem
     ///
-    LoadFromFile(file: string): Promise<Buffer>;
+    LoadFromFile(file: string): Promise<Uint8Array>;
 
     ///
     /// Connects to the Database
@@ -42,4 +40,32 @@ interface DataController {
     /// Disconnects from the database
     ///
     Disconnect(): Promise<void>;
+}
+
+export abstract class FsDataController implements DataController {
+    StoreInFile(file: string, data: Uint8Array): Promise<void> {
+        return Deno.writeFile(file, data)
+    }
+    LoadFromFile(file: string): Promise<Uint8Array> {
+        return Deno.readFile(file)    
+    }
+
+    abstract Connect(): Promise<void>;
+    abstract Query(query: string): Promise<object>;
+    abstract Disconnect(): Promise<void>;
+
+}
+
+
+// Don't implement a real data controller until we figure out a dataabse
+export class DummyDataController extends FsDataController {
+    Connect(): Promise<void> {
+        return Promise.resolve();
+    }
+    Query(query: string): Promise<object> {
+        return Promise.resolve({});
+    }
+    Disconnect(): Promise<void> {
+        return Promise.resolve();
+    }
 }
