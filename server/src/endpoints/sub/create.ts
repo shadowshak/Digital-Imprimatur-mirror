60bd6faec28ae31a586e2e89c0da30a628e2f2b1
Palladino, John <jpalladino002@student.franciscan.edu>
2023-02-27 21:23:31 +0000

    Move the server to the development branch

---
+++ server/src/endpoints/sub/create.ts
@@ -0,0 +1,74 @@
+import {
+    createRouter,
+    AugmentedRequest,
+    createRouteMap,
+    jsonResponse,
+  } from "https://deno.land/x/reno@v2.0.81/reno/mod.ts";
+import { BadJsonError, InvalidContentTypeError, InvalidMethodError, PayloadTooLargeError } from "../../errors.ts";
+import { AuthError } from "../../models/auth/Errors.ts";
+import { AccessToken } from "../../models/auth/Session.ts";
+  
+import { SubmissionId } from "../../models/submissions/Submission.ts";
+
+
+class SubCreateRequest {
+    token: AccessToken;
+    name: string;
+    description: string;
+
+    constructor(token: string, name: string, description: string) {
+        this.token = new AccessToken(token);
+        this.name = name;
+        this.description = description;
+    }
+
+    static from_json(json: any): SubCreateRequest | null {
+        if (!json) return null;
+
+        if (!("token" in json)) return null;
+        if (!("name" in json)) return null;
+        if (!("description" in json)) return null;
+
+        if (typeof json.token !== "string") return null;
+        if (typeof json.name !== "string") return null;
+        if (typeof json.description !== "string") return null;
+
+        return new SubCreateRequest(json.token, json.name, json.description);
+    }
+}
+
+export async function create(req: AugmentedRequest) {
+    if (req.method != "POST") {
+        throw new InvalidMethodError();
+    }
+    if (req.headers.get("Content-Type") != "application/json") {
+        throw new InvalidContentTypeError();
+    }
+
+    const body = await req.json();
+    const request = SubCreateRequest.from_json(body);
+
+    if (!request) {
+        throw new BadJsonError();
+    }
+
+    check_for_size_errors(request);
+
+    const response = {};
+
+    return jsonResponse(response);
+}
+
+const MAX_NAME_LENGTH = 100;
+const MAX_DESCRIPTION_LENGTH = 1000;
+
+function check_for_size_errors(req: SubCreateRequest) {
+    if (
+        req.name.length > MAX_NAME_LENGTH ||
+        req.description.length > MAX_DESCRIPTION_LENGTH
+    ) {
+        throw new PayloadTooLargeError();
+    }
+
+    
+}
---
+++ server/src/models/auth/Session.ts
@@ -0,0 +1,56 @@
+import * as uuid from "https://deno.land/std@0.175.0/uuid/mod.ts";
+
+import "./Capability.ts"
+import { Capabilities } from "./Capability.ts";
+
+export class AccessToken {
+    rawToken: string;
+
+    constructor(raw: string) {
+        this.rawToken = raw;
+    }
+
+    static Generate(): AccessToken {
+        return new AccessToken(uuid.v4.generate())
+    }
+}
+
+export class UserId {
+    rawId: string;
+
+    constructor(raw: string) {
+        this.rawId = raw;
+    }
+
+    static Generate(): UserId {
+        return new UserId(uuid.v4.generate())
+    }
+}
+
+export enum Role {
+    Admin,
+    Reviewer,
+    Publisher,
+}
+
+export class UserSession {
+    user_id:        UserId;
+    access_token:   AccessToken;
+    expiration:     Date;
+    permissions:    Capabilities;
+    role:           Role;
+
+    constructor(
+        user_id:        UserId,
+        access_token:   AccessToken,
+        expiration:     Date,
+        permissions:    Capabilities,
+        role:           Role)
+    {
+        this.user_id = user_id;
+        this.access_token = access_token;
+        this.expiration = expiration;
+        this.permissions = permissions;
+        this.role = role;
+    }
+}
