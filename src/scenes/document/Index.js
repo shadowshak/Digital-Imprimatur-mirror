2fc8fe16f4fbfd68a8c9990068243e0688f5ffd2
Sam Shadle <samshadle@stpaulcenter.com>
2023-02-27 16:29:43 -0500

    remove console log

--- src/scenes/document/Index.js
+++ src/scenes/document/Index.js
@@ -1,9 +1,7 @@
 import React from "react";
 
 function Document({ role }) {
-  return (
-    <div>console.log(role) Coming soon: the Document view for {role}s!</div>
-  );
+  return <div>Coming soon: the Document view for {role}s!</div>;
 }
 
 export default Document;
