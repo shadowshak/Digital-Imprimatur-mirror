0e4f1fbb2031b0e76f854bbc8949dc76397ea9d0
Sam Shadle <samshadle@stpaulcenter.com>
2023-02-27 15:34:45 -0500

    Add router, scenes/routes for /home, /document, and /login, and finish /home view

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import CssBaseline from "@mui/material/CssBaseline";
+import Document from "./scenes/document/Index";
+import Home from "./scenes/home/Index";
+import Login from "./scenes/login/Index";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <CssBaseline />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/document" element={<Document />} />
+      </Routes>
+    </Router>
   );
 }
 
